648bda3202b5f4e71686a2a03c49c4eaebe26448
jihukimme <jihu0210@naver.com>
2024-08-11 15:47:05 +0900

    first commit

---
+++ src/baekjoonLevel1.java
@@ -0,0 +1,182 @@
+//class 더하기 {
+////        Scanner scanner = new Scanner(System.in);
+////        int a = scanner.nextInt();
+////        int b = scanner.nextInt();
+//
+//        public void plus(int a, int b) {
+//            System.out.println(a + b);
+//        }
+//}
+
+//class 빼기 {
+////    Scanner scanner = new Scanner(System.in);
+////    int a = scanner.nextInt();
+////    int b = scanner.nextInt();
+//
+//    public void minus(int a, int b) {
+//        System.out.println(a - b);
+//    }
+//
+//}
+
+//class 곱하기 {
+////    Scanner scanner = new Scanner(System.in);
+////    int a = scanner.nextInt();
+////    int b = scanner.nextInt();
+//
+//    public void multiply(int a, int b) {
+//        System.out.println(a * b);
+//    }
+//}
+
+//class 나누기 {
+////    Scanner scanner = new Scanner(System.in);
+////    double a = scanner.nextInt();
+////    double b = scanner.nextInt();
+//
+//    public void divide(int a, int b) {
+//        System.out.println(a / b);
+//    }
+//}
+
+//class 나머지 {
+////    Scanner scanner = new Scanner(System.in);
+////    int a = scanner.nextInt();
+////    int b = scanner.nextInt();
+//
+//    public void remainder(int a, int b) {
+//        System.out.println(a % b);
+//    }
+//}
+
+//class existId {
+//    public void existId() {
+//        Scanner in = new Scanner(System.in);
+//        String id = in.next();
+//        System.out.println(id + "??!");
+//    }
+//}
+
+//class year {
+//    public void year() {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        int year = scanner.nextInt();
+//
+//        int yearChange = year - 543;
+//
+//        System.out.println(yearChange);
+//    }
+//}
+
+//class calculate {
+//    public void calculate() {
+//
+//        Scanner scanner = new Scanner(System.in);
+//
+//        int a = scanner.nextInt();
+//        int b = scanner.nextInt();
+//        int c = scanner.nextInt();
+//
+//        int result1 = (a + b) % c;
+//        int result2 = ((a % c) + (b % c)) % c;
+//        int result3 = (a * b) % c;
+//        int result4 = ((a % c) * (b % c)) % c;
+//
+//        System.out.println(result1);
+//        System.out.println(result2);
+//        System.out.println(result3);
+//        System.out.println(result4);
+//    }
+//}
+
+//class multiplyCalculate {
+//    public void multiplyCalculate() {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        int a = scanner.nextInt();
+//        int b = scanner.nextInt();
+//
+//        if(b < 10) {
+//            System.out.println(a * b);
+//        } else {
+//            System.out.println(a * (b % 10));
+//            System.out.println(a * ((b % 100) / 10));
+//            System.out.println(a * (b / 100));
+//            System.out.println(a * b);
+//        }
+//    }
+//}
+
+//class plusCalculate {
+//    public void plusCalculate() {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        Long a = scanner.nextLong();
+//        Long b = scanner.nextLong();
+//        Long c = scanner.nextLong();
+//
+//        Long sum = a + b +c;
+//
+//        System.out.println(sum);
+//    }
+//}
+
+//class cat {
+//    public void cat() {
+//        System.out.println("\\    /\\");
+//        System.out.println(" )  ( ')");
+//        System.out.println("(  /  )");
+//        System.out.println(" \\(__)|");
+//    }
+//}
+
+class dog {
+    public void dog() {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
+
+//class 정렬 {
+//    public void 정렬() {
+//        Scanner scanner = new Scanner(System.in);
+//
+//        int n = scanner.nextInt();
+//
+//        int[] arr = new int[n];
+//
+//        for (int i = 0; i < n; i++) {
+//            arr[i] = scanner.nextInt();
+//        }
+//
+//        for (int i = 0; i < n-1; i++) {
+//            for(int j = i+1; j<n; j++) {
+//                if (arr[i] > arr[j]) {
+//                    int temp = arr[i];
+//                    arr[i] = arr[j];
+//                    arr[j] = temp;
+//                }
+//            }
+//        }
+//
+//        for (int num : arr) {
+//            System.out.println(num);
+//        }
+//    }
+//
+//}
+
+public class baekjoonLevel1 {
+
+
+    public static void main(String[] args) {
+
+       dog dog = new dog();
+            dog.dog();
+
+    }
+}
---
+++ src/baekjoonLevel3.java
@@ -0,0 +1,313 @@
+import java.io.*;
+import java.util.StringTokenizer;
+
+//class multiplicationTable {
+//    public void multiplicationTable() {
+//
+//    }
+//
+//    public void multiplicationTable(int num) {
+//        for(int i=0; i<9; i++){
+//            System.out.println(num + " * " + (i + 1) + " = " + num*(i+1));
+//        }
+//    }
+//}
+
+//class plus {
+//
+//    public void plus() throws IOException{
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //scanner
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); //print
+//
+//        StringBuilder sb = new StringBuilder(); //make string
+//
+//        int num = Integer.parseInt(br.readLine());
+//
+//        int x;
+//        int y;
+//
+//        for(int i=0; i<num; i++){
+//            StringTokenizer st = new StringTokenizer(br.readLine(), " "); //string tokenizer
+//
+//            x = Integer.parseInt(st.nextToken());
+//            y = Integer.parseInt(st.nextToken());
+//
+//            sb.append(x + y);
+//            sb.append("\n");
+//        }
+//
+//        bw.write(String.valueOf(sb));
+//        bw.newLine();
+//
+//        bw.flush();
+//    }
+//}
+
+//class plus2 {
+//    public void plus2() throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //scanner
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); //print
+//
+////        StringTokenizer st = new StringTokenizer(br.readLine());
+//
+//        StringBuilder sb = new StringBuilder(); //make string
+//
+//        int num = Integer.parseInt(br.readLine());
+//        int sum = 0;
+//
+//        for(int i=0; i<num; i++){
+//            sum = sum + (i+1);
+//        }
+//
+////        sb.append(sum);
+//
+//        bw.write(String.valueOf(sum));
+//        bw.flush();
+//    }
+//}
+
+//class receipt {
+//    public void receipt() throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in)); //scanner
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out)); //print
+//
+////        StringTokenizer st = new StringTokenizer(br.readLine());
+//
+//        StringBuilder sb = new StringBuilder();
+//
+//        int totalMoney;
+//        int n;
+//        int itemMoney;
+//        int itemCount;
+//        int compareMoney = 0;
+//
+//        totalMoney = Integer.parseInt(br.readLine());
+//        n = Integer.parseInt(br.readLine());
+//
+//        for(int i=0; i<n; i++){
+//            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+//
+//            itemMoney = Integer.parseInt(st.nextToken());
+//            itemCount = Integer.parseInt(st.nextToken());
+//
+//            compareMoney = compareMoney + (itemMoney * itemCount);
+//        }
+//
+//        if(totalMoney == compareMoney){
+//            bw.write("Yes");
+//        } else {
+//            bw.write("No");
+//        }
+//        bw.flush();
+//    }
+//}
+
+//class dataType {
+//    public void dataType() throws IOException {
+//        // long int = 4바이트
+//        // long long int = 8바이트
+//        // long long long int = 12바이트
+//        // long long long long int = 16바이트
+//        // ...
+//
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//
+////        StringTokenizer st = new StringTokenizer(br.readLine());
+//
+//        StringBuilder sb = new StringBuilder();
+//
+//        int num = Integer.parseInt(br.readLine());
+//
+//        int printLong = num/4;
+//
+//        for(int i=0; i<printLong; i++){
+//            sb.append("long ");
+//        }
+//
+//        sb.append("int");
+//
+//        bw.write(String.valueOf(sb));
+//        bw.flush();
+//    }
+//}
+
+//class fastPlus {
+//    public void fastPlus() throws IOException {
+//
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//
+////        StringTokenizer st = new StringTokenizer(br.readLine());
+//
+//        StringBuilder sb = new StringBuilder();
+//
+//        int num = Integer.parseInt(br.readLine());
+//
+//        int a;
+//        int b;
+//
+//        for(int i=0; i<num; i++) {
+//            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+//
+//            a = Integer.parseInt(st.nextToken());
+//            b = Integer.parseInt(st.nextToken());
+//
+//            sb.append(a + b);
+//            sb.append("\n");
+//        }
+//
+//        bw.write(String.valueOf(sb));
+//        bw.flush();
+//
+//    }
+//}
+
+//class newPlus {
+//    public void newPlus() throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//
+////        StringTokenizer st = new StringTokenizer(br.readLine());
+//
+//        StringBuilder sb = new StringBuilder();
+//
+//        int num = Integer.parseInt(br.readLine());
+//
+//        int a;
+//        int b;
+//
+//        for(int i=0; i<num; i++){
+//            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+//
+//            a = Integer.parseInt(st.nextToken());
+//            b = Integer.parseInt(st.nextToken());
+//
+//            sb.append("Case #" + (i+1) + ": " + a + " " + "+ " + b + " " + "= ");
+//            sb.append(a + b);
+//            sb.append("\n");
+//        }
+//
+//        bw.write(String.valueOf(sb));
+//        bw.flush();
+//    }
+//}
+
+//class star1 {
+//    public void star1() throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//
+////        StringTokenizer st = new StringTokenizer(br.readLine());
+//
+//        StringBuilder sb = new StringBuilder();
+//
+//        int num = Integer.parseInt(br.readLine());
+//
+//        for(int i=0; i<num; i++){
+//            sb.append("*");
+//
+//            for(int j = 0; j < i; j++){
+//                sb.append("*");
+//            }
+//            sb.append("\n");
+//        }
+//
+//        bw.write(String.valueOf(sb));
+//        bw.flush();
+//
+//    }
+//}
+
+//class star2 {
+//    public void star2() throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//
+////        StringTokenizer st = new StringTokenizer(br.readLine());
+//
+//        StringBuilder sb = new StringBuilder();
+//
+//        int num = Integer.parseInt(br.readLine());
+//
+//        for(int i=0; i<num; i++) {
+//            for(int j=i+1; j<num; j++){
+//                sb.append(" ");
+//            }
+//            for(int z=0; z<=i; z++){
+//                sb.append("*");
+//            }
+//            sb.append("\n");
+//        }
+//
+//        bw.write(String.valueOf(sb));
+//        bw.flush();
+//    }
+//}
+
+//class plus5 {
+//    public void plus5() throws IOException {
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//
+////        StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+//
+//        StringBuilder sb = new StringBuilder();
+//
+//        int a;
+//        int b;
+//
+//        do{
+//            StringTokenizer st = new StringTokenizer(br.readLine(), " ");
+//
+//            a = Integer.parseInt(st.nextToken());
+//            b = Integer.parseInt(st.nextToken());
+//
+//            if(a != 0 && b !=0){
+//                sb.append(a+b);
+//                sb.append("\n");
+//            }
+//
+//        }while(a != 0 && b != 0);
+//
+//
+//        bw.write(String.valueOf(sb));
+//        bw.flush();
+//    }
+//}
+
+class plus4 {
+    public void plus4() throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+//        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        StringBuilder sb = new StringBuilder();
+
+        String str;
+
+        while((str = br.readLine()) != null){
+            StringTokenizer st = new StringTokenizer(str, " ");
+
+            int a = Integer.parseInt(st.nextToken());
+            int b = Integer.parseInt(st.nextToken());
+
+            sb.append(a + b);
+            sb.append("\n");
+        }
+
+        bw.write(String.valueOf(sb));
+        bw.flush();
+    }
+}
+
+public class baekjoonLevel3 {
+
+    public static void main(String[] args) throws IOException {
+
+        plus4 plus = new plus4();
+        plus.plus4();
+
+    }
+}
